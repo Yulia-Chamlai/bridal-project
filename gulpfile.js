c904702623bfc64fcf11c1e08f0b28ad45de74ab
Yulia-Chamlai <yuliachamali@gmail.com>
2026-04-06 21:53:35 +0300

    added section top and promo

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ function fonts() {
 function images() {
   return src(["app/images/src/*.*", "!app/images/src/*.svg"])
     .pipe(newer("app/images"))
-    .pipe(avif({ quality: 50 }))
+    .pipe(avif({ quality: 75 }))
 
     .pipe(src("app/images/src/*.*"))
     .pipe(newer("app/images"))
