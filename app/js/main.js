36c843f8db7ebe4b2ebf5c0831be62a79e6cea2e
Yulia-Chamlai <yuliachamali@gmail.com>
2026-04-15 12:46:59 +0300

    added accessories section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,9 @@
-
+const swiper = new Swiper(".accessories__slider", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 40,
+  navigation: {
+    nextEl: ".accessories__btn-next",
+    prevEl: ".accessories__btn-prev",
+  },
+});
